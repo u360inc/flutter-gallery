786520d0b3d6e6f1ea4d70f103e5d7c77cfa03cb
ENDOH takanao <djmchl@gmail.com>
2018-01-05 11:41:18 +0700

    move to gallery

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_gallery/main.dart';
+import 'package:flutter_gallery/gallery/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
